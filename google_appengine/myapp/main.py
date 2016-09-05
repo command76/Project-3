8c75129a8b738ff6e099ef7f5fcd81a8dcb57b78
Anthony Taveras <urawesomeman@hotmail.com>
2016-09-05 01:58:24 -0400

    Add not fully operating update to shopping list template

--- google_appengine/myapp/main.py
+++ google_appengine/myapp/main.py
@@ -15,97 +15,69 @@
 # limitations under the License.
 #
 
-#Practice code to review
-
-months = ['January','February','March','April','May','June','July','August','September','October','November','December']
-
-month_abbvs = dict((m[:3].lower(),m) for m in months)
-
-def valid_month(month):
-    if month:
-        short_month = month[:3].lower()
-        return month_abbvs.get(short_month)
-
-def valid_day(day):
-    if day and day.isdigit():
-        day = int(day)
-        if day > 0 and day <= 31:
-            return day
-
-def valid_year(year):
-    if year and year.isdigit():
-        year = int(year)
-        if year > 1900 and year < 2020:
-            return year
-
-import cgi
-def escape_html(s):
-    return cgi.escape(s, quote = True)
-
-
-form="""
-<form method="post">
-    What is your birthday?
-    <br>
-	<label>
-        Month 
-        <input type="text" name="month" value="%(month)s">
-    </label>
-	<label>
-        Day 
-        <input type="text" name="day" value="%(day)s">
-    </label>
-    <label>
-        Year 
-        <input type="text" name="year" value="%(year)s">
-    </label>
-    <div style="color: red">%(error)s</div>
-    <br>
-    <br>
-    <input type="submit">
-</form>
-"""
-
+import os
+import jinja2
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def write_form(self, error="", month="", day="", year=""):
-        self.response.out.write(form % {"error": error,
-                                        "month": escape_html(month),
-                                        "day": escape_html(day),
-                                        "year": escape_html(year)})
 
+template_dir = os.path.join(os.path.dirname(__file__), 'templates') #concatenates 2 file names.  The template directory is the directory current file is in and adding templetes to it
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+# hidden_html = """
+# <input type="hidden" name="food" value="%s">
+# """
+
+# item_html = "<li>%s</li>"
+
+# shopping_list_html = """
+# <br>
+# <br>
+# <h2>Shopping List</h2>
+# <ul>
+# %s
+# </ul>
+# """
+
+class Handler(webapp2.RequestHandler): #MainHandler class is defined here
+    def write(self, *a, **kw): #method write lets print a string without typing self.response.out.write but with just self.write
+        self.response.out.write(*a, **kw) #the default method is get which means when a form is submitted the perameter is added to the URL
+
+    def render_str(self, template, **params): #takes file name and a bunch of extra parameters
+        t = jinja_env.get_template(template) #use jinja parameter.  Call get_template and give it a filename file is loaded and stored in jinja template called t
+        return t.render(params) #t.render passes in parameters that were passed into that function
+
+    def render(self, template, **kw): #takes template and a bunch of extra parameters
+        self.write(self.render_str(template, **kw)) #calls render_str and wraps it in self.write sends it back to the browser
+
+class MainPage(Handler): #inherits from a class called MainHandler
     def get(self):
-        self.write_form()
+        items = self.request.get_all("food") #gets all of the get perameters or post perameters a list of all the food perameters
+        self.render("shopping_list.html", items = items)
 
-    def post(self):
-        user_month = self.request.get('month')
-        user_day = self.request.get('day')
-        user_year = self.request.get('year')
 
-        month = valid_month(user_month)
-        day = valid_day(user_day)
-        year = valid_year(user_year)
+        # output = form_html
+        # output_hidden = "" #holding place for hidden inputs
 
-        if not (month and day and year):
-            self.write_form("That doesn't look valid to me, friend.",
-                             user_month, user_day, user_year)
-        else:
-            self.redirect("/thanks")
-            
+       
+        # if items:
+        #     output_items = ""
+        #     for item in items:
+        #         output_hidden += hidden_html % item # for each item in items add to the string hidden html subsituting the food name
+        #         output_items += item_html % item
 
-class ThanksHandler(webapp2.RequestHandler):
+        #     output_shopping = shopping_list_html % output_items
+        #     output += output_shopping
+
+        # output = output % output_hidden
+
+
+        # self.write(output) #if you want to make a basic form you can just return html directly from the Handler here.
+
+class FizzBuzzHandler(Handler):
     def get(self):
-        self.response.out.write("Thanks! That's a totally valid day!")
-
-
-#class Testhandler(webapp2.RequestHandler):
-    #def post(self):
-    	#q = self.request.get("q")
-        #self.response.out.write(q)
-
-        #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.out.write(self.request)
+        n = self.request.get('n', 0)
+        n = n and int(n)
+        self.render('fizzbuzz.html', n = n)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),('/thanks', ThanksHandler)], debug=True)
+    ('/', MainPage),('/fizzbuzz', FizzBuzzHandler)], debug=True)
